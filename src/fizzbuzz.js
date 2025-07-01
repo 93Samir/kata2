a07b461de73fb6141b4cfe669e9a0f06abe14ec3
Samir Kouhnaward <samirxkouhnaward@gmail.com>
2025-07-01 23:59:25 +0200

    feat: return 'Fizz' for input 3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,14 @@
 function fizzBuzz(n) {
+    if (n === 3) return "Fizz";
     return n;
 }
+
 
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = fizzBuzz;
 }
+
+it("returns 'Fizz' for 3", function () {
+    expect(fizzBuzz(3)).toBe("Fizz");
+});
+
